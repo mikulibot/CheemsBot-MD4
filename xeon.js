ceb6dc66369089161f2e2f6047292cbcc47f9860
Raizel-2023 <111248812+Raizel-2023@users.noreply.github.com>
2022-09-28 09:35:38 +0300

    Update xeon.js

--- xeon.js
+++ xeon.js
@@ -233,7 +233,7 @@ XeonBotInc.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
    //dont forget to put my name(Xeon) as credit
    //you fail to put, i sue you for sure!
 let buttons = [
-{buttonId: `wkwkwk`, buttonText: {displayText: 'مع السلامه يحب كنت منورنا بوجودك☺️♥🥀'}, type: 1}
+{buttonId: `wkwkwk`, buttonText: {displayText: 'تطلع يجي غيرك '}, type: 1}
 ]
 let buttonMessage = {
 document: fs.readFileSync('./XeonMedia/theme/cheems.xlsx'),
